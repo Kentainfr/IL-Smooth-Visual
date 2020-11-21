dda7ce9d5bf05b1574b2daffd06788c597250ead
Kentain_fr <63867494+Kentainfr@users.noreply.github.com>
2020-11-21 01:12:07 +0100

    Add files via upload
    Content

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+files {
+    'index.html',
+    'css/style.css',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/logo.png',
+    'js/script.js'
+}
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
